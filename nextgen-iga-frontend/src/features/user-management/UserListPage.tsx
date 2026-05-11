0ee495bcb03dd576f0ee2713f54f7fa1bc39e41b
Abhik4004 <ghosh.abhik.2004@gmail.com>
2026-05-11 13:18:01 +0530

    useMemo removed

--- nextgen-iga-frontend/src/features/user-management/UserListPage.tsx
+++ nextgen-iga-frontend/src/features/user-management/UserListPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState} from 'react';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import { Search, ChevronDown, ChevronUp, Users, Shield, UserCheck, Layers, Mail, Crown } from 'lucide-react';
